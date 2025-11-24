bb3d742eb792df7074d4990f8eddd49e88565b8a
asker <askersultanov666@gmail.com>
2025-11-24 22:31:15 +0300

    deleted session creation into "reportProcessing" func

--- src/routes/index/utils/reportProcessing.js
+++ src/routes/index/utils/reportProcessing.js
@@ -1,48 +1,34 @@
 var wbapi = require("./WBAPI");
 var sortYearsTree = require("./sortYearTree");
 var dbUtils = require("../../../database/utils");
-var { connection } = require("../../../database");
 var insertReportToReportTree = require("./reportTreeBuilder");
 var parseReports = require("./writeAndCalcReportDataFromWBAPI");
 
-var reportProcessing = async (userId, dateFrom, dateTo, token) => {
-  var session = connection.startSession();
+var reportProcessing = async (userId, dateFrom, dateTo, token, session) => {
+  var reports = await wbapi.getReports(userId, dateFrom, dateTo, token);
+  var reportId = reports.weeklyFinancialReport[0].realizationreport_id;
 
-  try {
-    session.startTransaction();
+  var { reportTree } = await dbUtils.getReportsTree(userId);
+  var { years, year, month } = await insertReportToReportTree(dateFrom, dateTo, reportId, reportTree);
+  var sortedYears = sortYearsTree(years);
+  await dbUtils.updateReportTree(userId, sortedYears);
 
-    var reports = await wbapi.getReports(userId, dateFrom, dateTo, token);
-    var reportId = reports.weeklyFinancialReport[0].realizationreport_id;
+  var { taxRate, paidTaxAmount } = await dbUtils.addNewTaxYearToDb(userId, +year, session);
 
-    var { reportTree } = await dbUtils.getReportsTree(userId);
-    var { years, year, month } = await insertReportToReportTree(dateFrom, dateTo, reportId, reportTree);
-    var sortedYears = sortYearsTree(years);
-    await dbUtils.updateReportTree(userId, sortedYears);
+  var { report } = await parseReports(taxRate, reports);
 
-    var { taxRate, paidTaxAmount } = await dbUtils.addNewTaxYearToDb(userId, +year, session);
+  paidTaxAmount += report.totalTaxAmount;
+  await dbUtils.changePaidTaxAmountToDb(userId, year, paidTaxAmount, session);
 
-    var { report } = await parseReports(taxRate, reports);
+  report.dateTo = dateTo;
+  report.userId = userId;
+  report.taxRate = taxRate;
+  report.dateFrom = dateFrom;
+  report.reportId = reportId;
+  report.recordTo = { year, month };
 
-    paidTaxAmount += report.totalTaxAmount;
-    await dbUtils.changePaidTaxAmountToDb(userId, year, paidTaxAmount, session);
-
-    report.dateTo = dateTo;
-    report.userId = userId;
-    report.taxRate = taxRate;
-    report.dateFrom = dateFrom;
-    report.reportId = reportId;
-    report.recordTo = { year, month };
-
-    var success = await dbUtils.saveReportToDb(userId, report, session);
-    console.log({ success });
-
-    await session.commitTransaction();
-  } catch (e) {
-    await session.abortTransaction();
-    throw e;
-  } finally {
-    await session.endSession();
-  }
+  var success = await dbUtils.saveReportToDb(userId, report, session);
+  console.log({ success });
 };
 
 module.exports = reportProcessing;
