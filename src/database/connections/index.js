50905c2fe352b86ea4d1c3bb49df54d8397b28bc
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-11-28 14:25:41 +0300

    added env DB_NAME, implemented into src\database\connections\index.js

--- src/database/connections/index.js
+++ src/database/connections/index.js
@@ -1,9 +1,9 @@
 var { connection } = require("../index");
 
-var tokens_collection = connection.db("financial-reports").collection("tokens");
-var reports_collection = connection.db("financial-reports").collection("reports");
-var tax_params_collection = connection.db("financial-reports").collection("tax_params");
-var reports_tree_collection = connection.db("financial-reports").collection("reports_trees");
-var report_loading_states_collection = connection.db("financial-reports").collection("report_loading_states");
+var tokens_collection = connection.db(process.env.DB_NAME).collection("tokens");
+var reports_collection = connection.db(process.env.DB_NAME).collection("reports");
+var tax_params_collection = connection.db(process.env.DB_NAME).collection("tax_params");
+var reports_tree_collection = connection.db(process.env.DB_NAME).collection("reports_trees");
+var report_loading_states_collection = connection.db(process.env.DB_NAME).collection("report_loading_states");
 
 module.exports = { tokens_collection, tax_params_collection, reports_collection, reports_tree_collection, report_loading_states_collection };
