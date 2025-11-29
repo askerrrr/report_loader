463851dff2e65e7003ad30c21c26cdc8ecd1cf0e
asker <askersultanov666@gmail.com>
2025-11-29 19:14:28 +0300

    fixed err into src/routes/index/utils/WBAPI/reports/index.js

--- src/routes/index/utils/WBAPI/reports/index.js
+++ src/routes/index/utils/WBAPI/reports/index.js
@@ -5,6 +5,8 @@ var getAdvertisingCostsForPeriod = require("./getAdvertisingCostsForPeriod");
 var checkPaidStorageReportCreationStatus = require("./checkPaidStorageReportCreationStatus");
 var getPaidStorageReportByTaskIdFromWBAPI = require("./getPaidStorageReportByTaskIdFromWBAPI");
 
+var noDataForPeriodMessage = "there is no data available for the selected reporting period";
+
 var getReports = async (userId, dateFrom, dateTo, token) => {
   var { taskId } = await createPaidStorageReportTask(dateFrom, dateTo, token, userId);
 
@@ -20,11 +22,8 @@ var getReports = async (userId, dateFrom, dateTo, token) => {
     getAdvertisingCostsForPeriod(dateFrom, dateTo, token, userId),
   ]);
 
-  if (
-    [weeklyFinancialReport, paidStorageReport].every((report) => report.length === 0) &&
-    totalAdvertisingCosts === 0
-  ) {
-    throw new WBAPIError(userId, 404, "Нет отчетов за выбранный период");
+  if ([weeklyFinancialReport, paidStorageReport].every((report) => report.length === 0) && totalAdvertisingCosts === 0) {
+    throw new Error(noDataForPeriodMessage);
   }
 
   return { weeklyFinancialReport, paidStorageReport, totalAdvertisingCosts };
