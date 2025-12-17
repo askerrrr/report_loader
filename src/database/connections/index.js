7a77f5cfb03f9a9154e37cd3778c8b83c1a4d85d
Asker Sultanov <asker_sultanov1999@mail.ru>
2025-12-17 10:02:20 +0300

    added obj of goods_collection

--- src/database/connections/index.js
+++ src/database/connections/index.js
@@ -1,9 +1,17 @@
 var { connection } = require("../index");
 
+var goods_collection = connection.db(process.env.DB_NAME).collection("goods");
 var tokens_collection = connection.db(process.env.DB_NAME).collection("tokens");
 var reports_collection = connection.db(process.env.DB_NAME).collection("reports");
 var tax_params_collection = connection.db(process.env.DB_NAME).collection("tax_params");
 var reports_tree_collection = connection.db(process.env.DB_NAME).collection("reports_trees");
 var report_loading_states_collection = connection.db(process.env.DB_NAME).collection("report_loading_states");
 
-module.exports = { tokens_collection, tax_params_collection, reports_collection, reports_tree_collection, report_loading_states_collection };
+module.exports = {
+  goods_collection,
+  tokens_collection,
+  reports_collection,
+  tax_params_collection,
+  reports_tree_collection,
+  report_loading_states_collection,
+};
