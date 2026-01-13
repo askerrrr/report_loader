4805ac3e07fb8394c670bd9f2fad3183cc4091c8
asker <askersultanov666@gmail.com>
2026-01-13 19:53:24 +0300

    removed second arg "null" for "findOne" method into "addNewTaxYearToDb" db util

--- src/database/utils/addNewTaxYear.js
+++ src/database/utils/addNewTaxYear.js
@@ -3,7 +3,7 @@ var defaultTaxParams = require("../defaultTaxParams");
 
 var addNewTaxYearToDb = async (collection, userId, year, session) => {
   try {
-    var data = await collection.findOne({ userId }, null, { session: session });
+    var data = await collection.findOne({ userId }, { session: session });
     var taxYears = data.toObject().years;
 
     var existTaxParams = taxYears.find((params) => params.year === year);
