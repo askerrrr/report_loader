6e0a3f3fb075b2157aca06509d07a06b30a59089
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-01-19 15:04:48 +0300

    updated db util getToken

--- src/database/utils/getToken.js
+++ src/database/utils/getToken.js
@@ -2,7 +2,14 @@ var { DatabaseError } = require("../../customError");
 
 var getToken = async (collection, userId, session) => {
   try {
-    var { token } = await collection.findOne({ userId }, { session: session });
+    var token;
+
+    if (session) {
+      token = await collection.findOne({ userId }, { session: session });
+    } else {
+      token = await collection.findOne({ userId });
+    }
+
     return { token };
   } catch (e) {
     throw new DatabaseError(userId, e);
