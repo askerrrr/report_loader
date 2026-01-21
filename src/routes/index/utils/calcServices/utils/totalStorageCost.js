3cec982dd87c11a73fc90d89d86ea592dc561989
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-01-21 14:04:34 +0300

    used func truncateNum into calcTotalStorageCost

--- src/routes/index/utils/calcServices/utils/totalStorageCost.js
+++ src/routes/index/utils/calcServices/utils/totalStorageCost.js
@@ -1,3 +1,8 @@
-var calcTotalStorageCost = async (report) => report.reduce((acc, sku) => acc + sku.storage_fee, 0);
+var truncateNum = require("../../reportParsing/truncateNum");
+
+var calcTotalStorageCost = async (report) => {
+  var totalStorageCost = report.reduce((acc, sku) => acc + sku.storage_fee, 0);
+  return truncateNum(totalStorageCost);
+};
 
 module.exports = calcTotalStorageCost;
