740e0c15631e804a9cb88986038119c75b96ae31
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-01-21 13:36:09 +0300

    added && implemented file/func resumeInterruptedReportsLoad

---
+++ src/routes/index/utils/resumeInterruptedReportsLoad.js
@@ -0,0 +1,32 @@
+var loader = require("./loader");
+var dbUtils = require("../../../database/utils");
+var { connection } = require("../../../database");
+
+var killAllSessions = async () =>
+  connection
+    .db("admin")
+    .command({ killAllSessions: [] })
+    .then(() => console.log("all sessions killed"));
+
+var resumeInterruptedReportsLoad = async () => {
+  await killAllSessions();
+
+  var users = await dbUtils.getUsersData();
+
+  console.log({ users });
+
+  if (!users.length) {
+    return;
+  }
+
+  for (var { userId, reportsQueue } of users) {
+    if (!reportsQueue.length) {
+      continue;
+    }
+
+    var { token } = await dbUtils.getToken(userId);
+    await loader(userId, token);
+  }
+};
+
+module.exports = resumeInterruptedReportsLoad;
