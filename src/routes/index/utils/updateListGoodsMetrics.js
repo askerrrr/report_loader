03ea64d50d8f6baf9226586a4241213834ee8877
asker <askersultanov666@gmail.com>
2026-01-31 14:46:35 +0300

    fixed func "updateListGoodsMetrics"

--- src/routes/index/utils/updateListGoodsMetrics.js
+++ src/routes/index/utils/updateListGoodsMetrics.js
@@ -46,7 +46,7 @@ var updateListGoodsMetrics = async (report, listGoods) => {
     }
   }
 
-  return { listGoodsWithUpdatedSkuMetrics };
+  return { listGoodsWithUpdatedSkuMetrics: listGoods };
 };
 
 module.exports = updateListGoodsMetrics;
