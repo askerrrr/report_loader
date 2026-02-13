88f68b2f9c2492e979adfb936ec545956e038fd9
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-02-13 15:02:07 +0300

    updated nextReportDelay func, added arg delayMs

--- src/routes/index/utils/loader.js
+++ src/routes/index/utils/loader.js
@@ -5,7 +5,7 @@ var reportsProcessing = require("./reportsProcessing");
 var MAX_FAILED_ATTEMPTS = 3;
 var NEXT_REPORT_DELAY_MS = 65000;
 var noDataForPeriodMessage = "there is no data available for the selected reporting period";
-var nextReportDelay = async () => new Promise((res) => setTimeout(res, NEXT_REPORT_DELAY_MS));
+var nextReportDelay = async (delayMs) => new Promise((res) => (delayMs ? setTimeout(res, delayMs) : setTimeout(res, NEXT_REPORT_DELAY_MS)));
 
 var loader = async (userId, token) => {
   await dbUtils.setLoadingProgressStatus(userId, "loading").then(() => console.log("loading is started"));
