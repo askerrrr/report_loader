7f8f7a57d23d6b430b079d815592ba5c99cbbbca
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-02-20 15:08:30 +0300

    in the writeReportsToQueue controller, I implemented a delay in loading the report, provided that 60 seconds have not passed since the last report was loaded

--- src/routes/index/controllers/writeReportsToQueue.js
+++ src/routes/index/controllers/writeReportsToQueue.js
@@ -2,18 +2,30 @@ var dbUtils = require("../../../database/utils");
 
 var writeReportsToQueue = async (req, res, next) => {
   var { userId, filteredRequiredReportPeriods } = req.body;
-
   await dbUtils.pushToReportsQueue(userId, filteredRequiredReportPeriods);
 
-  var { loadingInProgress } = await dbUtils.getReportLoadingState(userId);
-
+  var { loadingInProgress, isReportLoadingDelayed } = await dbUtils.getReportLoadingState(userId);
   if (loadingInProgress) {
     return res.sendStatus(200);
   }
 
+  if (isReportLoadingDelayed) {
+    return res.sendStatus(202);
+  }
+
   res.sendStatus(202);
+
+  if (req.body.needsReportLoadingDelay) {
+    await dbUtils.updateReportLoadingDelayStatus(userId, true);
+    await delay(req.body.nextRequestDelayMs);
+    await dbUtils.updateReportLoadingDelayStatus(userId, false);
+  }
 
   next();
 };
 
 module.exports = writeReportsToQueue;
+
+var delay = async function (ms) {
+  return new Promise((res) => setTimeout(res, ms));
+};
