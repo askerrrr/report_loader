09b21c15dac3fbf68461f68ff59b617cde1378da
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-04-28 15:08:02 +0300

    Updated the getUsersData function:  - moved the projection setting to an object variable  - added the freshReportPeriodIndex field to the projection object

--- src/database/utils/getUsersData.js
+++ src/database/utils/getUsersData.js
@@ -1,10 +1,18 @@
 import { DatabaseError } from "../../customError/index.js";
 
+var projection = {
+  _id: 0,
+  userId: 1,
+  loadingInProgress: 1,
+  reportsQueue: 1,
+  abandonedReports: 1,
+  isReportLoadingDelayed: 1,
+  freshReportPeriodIndex: 1,
+};
+
 var getUsersData = async (collection) => {
   try {
-    var data = await collection
-      .find({}, { projection: { _id: 0, userId: 1, loadingInProgress: 1, reportsQueue: 1, abandonedReports: 1, isReportLoadingDelayed: 1 } })
-      .toArray();
+    var data = await collection.find({}, { projection }).toArray();
     return data;
   } catch (e) {
     throw new DatabaseError(userId, e);
