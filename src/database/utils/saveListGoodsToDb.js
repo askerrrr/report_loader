03e192ef05881e348d1d2bc45bcf1400eb6998b5
asker <askersultanov666@gmail.com>
2026-04-30 22:47:04 +0300

    removed block try/catch from "saveListGoodsToDb" db function

--- src/database/utils/saveListGoodsToDb.js
+++ src/database/utils/saveListGoodsToDb.js
@@ -1,19 +1,4 @@
-import { WBAPIError } from "../../customError/index.js";
-
-var saveListGoodsToDb = async (collection, userId, listGoods, session) => {
-  try {
-    var result;
-
-    if (session) {
-      result = await collection.updateOne({ userId }, { $set: { listGoods } }, { session: session });
-    } else {
-      result = await collection.updateOne({ userId }, { $set: { listGoods } });
-    }
-
-    return result;
-  } catch (e) {
-    throw new WBAPIError(userId, 500, e);
-  }
-};
+var saveListGoodsToDb = async (collection, userId, listGoods, session) =>
+  await collection.updateOne({ userId }, { $set: { listGoods } }, { session });
 
 export default saveListGoodsToDb;
