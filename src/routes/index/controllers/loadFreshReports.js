d52c5d426083e82ef5c4f71fc35f36b240c6bbc4
asker <askersultanov666@gmail.com>
2026-05-19 22:30:28 +0300

    used db util "updateLastReportRequestTimestamp" into "loadFreshReports" controller

--- src/routes/index/controllers/loadFreshReports.js
+++ src/routes/index/controllers/loadFreshReports.js
@@ -70,6 +70,7 @@ var loadFreshReports = async (req, res, next) => {
         try {
           var { dateFrom, dateTo } = reportPeriodToLoad;
           await reportsProcessing(userId, dateFrom, dateTo, session);
+          await dbUtils.updateLastReportRequestTimestamp(userId, session);
           await dbUtils.updateFreshReportPeriodIndex(userId, nextReportPeriodIndex, session);
         } catch (processingError) {
           if (processingError.message === noDataForPeriodMessage) {
