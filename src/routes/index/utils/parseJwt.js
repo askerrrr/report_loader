c3b6a98ed0b4606787c7f8f6d01499633a974e17
asker <askersultanov666@gmail.com>
2026-05-23 15:21:24 +0300

    fixed parseJwt function exporting

--- src/routes/index/utils/parseJwt.js
+++ src/routes/index/utils/parseJwt.js
@@ -6,4 +6,5 @@
 // Posted by Peheje, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-05-23, License - CC BY-SA 4.0
 
-export default parseJwt = (token) => JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
+var parseJwt = (token) => JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
+export default parseJwt;
