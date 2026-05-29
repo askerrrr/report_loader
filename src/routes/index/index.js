8f6012dcbc347b3054291e77d3503ea72e1d0862
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-05-29 14:44:26 +0300

    Added url path /resume-loading/ and used controller resumeReportLoading on new url path

--- src/routes/index/index.js
+++ src/routes/index/index.js
@@ -4,12 +4,15 @@ import requestBodySchema from "./requestBodySchema.js";
 import periodsFilter from "./controllers/periodsFilter.js";
 import reportLoading from "./controllers/reportLoading.js";
 import loadFreshReports from "./controllers/loadFreshReports.js";
+import resumeReportLoading from "./controllers/resumeReportLoading.js";
 import writeReportsToQueue from "./controllers/writeReportsToQueue.js";
 import joiSchemaValidator from "../../middleware/joiSchemaValidator.js";
 
 var router = Router({ caseSensitive: true });
 
 router.post("/", joiSchemaValidator(requestBodySchema), checkAuth, periodsFilter, writeReportsToQueue, reportLoading);
+
+router.post("/resume-loading/", resumeReportLoading);
 
 router.post("/background-tasks/load-fresh-reports", loadFreshReports);
 
