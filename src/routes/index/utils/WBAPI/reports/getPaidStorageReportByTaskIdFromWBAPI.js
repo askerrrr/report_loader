1a4499c62835b5d89f67301476e4b1eb32a2c343
asker <askersultanov666@gmail.com>
2026-05-31 15:57:58 +0300

    updated "getPaidStorageReportByTaskIdFromWBAPI" wbapi service

--- src/routes/index/utils/WBAPI/reports/getPaidStorageReportByTaskIdFromWBAPI.js
+++ src/routes/index/utils/WBAPI/reports/getPaidStorageReportByTaskIdFromWBAPI.js
@@ -1,52 +1,45 @@
 import { WBAPIError } from "../../../../../customError/index.js";
 
 var getPaidStorageReportByTaskIdFromWBAPI = async (taskId, token, userId) => {
-  try {
-    var url = `https://seller-analytics-api.wildberries.ru/api/v1/paid_storage/tasks/${taskId}/download`;
+  var url = `https://seller-analytics-api.wildberries.ru/api/v1/paid_storage/tasks/${taskId}/download`;
 
-    var res = await fetch(url, {
-      method: "GET",
-      headers: {
-        Authorization: "Bearer " + token,
-      },
-    });
+  var res = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
 
-    if (res.status === 204) {
-      return [];
-    } else if (res.ok) {
-      var paidStorageReport = await res.json();
-      return paidStorageReport;
-    }
-
-    var errMsg;
-
-    if (res.status === 429) {
-    } else if (res.status === 401) {
-      errMsg =
-        "Не удалось авторизоваться для получения отчета о платном хранении с помощью сохраненного токена. Получить токен с нужными правами можно получить в личном кабинете продавца";
-    }
-
-    switch (res.status) {
-      case 400:
-        errMsg = "Неправильный запрос";
-        break;
-      case 401:
-        errMsg = "Не удалось авторизоваться для получения отчета о платном хранении с помощью сохраненного токена";
-        break;
-      case 429:
-        errMsg = "Подождите минуту перед получением нового отчёта о платном хранении";
-        break;
-      case 402:
-        errMsg = "Требуется платеж";
-        break;
-      default:
-        errMsg = "Возникла ошибка при получении отчета о платном хранении, попробуйте позже";
-    }
-
-    throw new WBAPIError(userId, res.status, errMsg);
-  } catch (e) {
-    throw new WBAPIError(userId, 500, "Не удалось подключиться к WBAPI" + "\n" + e.message);
+  if (res.status === 204) {
+    return [];
   }
+
+  if (res.ok) {
+    var paidStorageReport = await res.json();
+
+    return paidStorageReport;
+  }
+
+  var errMsg;
+
+  switch (res.status) {
+    case 400:
+      errMsg = "Неправильный запрос";
+      break;
+    case 401:
+      errMsg = "Не удалось авторизоваться для получения отчета о платном хранении с помощью сохраненного токена";
+      break;
+    case 429:
+      errMsg = "Подождите минуту перед получением нового отчёта о платном хранении";
+      break;
+    case 402:
+      errMsg = "Требуется платеж";
+      break;
+    default:
+      errMsg = "Возникла ошибка при получении отчета о платном хранении, попробуйте позже";
+  }
+
+  throw new WBAPIError(userId, res.status, errMsg);
 };
 
 export default getPaidStorageReportByTaskIdFromWBAPI;
