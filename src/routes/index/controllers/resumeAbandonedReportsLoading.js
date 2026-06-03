5fb0324431d28c1a3c342f37c2b22e0091cbda8c
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-03 15:23:21 +0300

    added new controller resumeAbandonedReportsLoading

---
+++ src/routes/index/controllers/resumeAbandonedReportsLoading.js
@@ -0,0 +1,38 @@
+import Joi from "joi";
+import loader from "../utils/loader.js";
+
+var isServerStartupLoad = false;
+
+var abandonedReportPeriodsSchema = Joi.array().items({
+  index: Joi.number().required(),
+  dateTo: Joi.string().required(),
+  dateFrom: Joi.string().required(),
+  failedCount: Joi.number().required(),
+});
+
+var schema = Joi.object({ userId: Joi.string().required(), abandonedReportPeriods: abandonedReportPeriodsSchema });
+
+var resumeAbandonedReportsLoading = async () => {
+  var authHeader = req.headers?.authorization;
+
+  if (!authHeader) {
+    return res.sendStatus(401);
+  }
+
+  var [type, secretKey] = authHeader.split(" ");
+
+  if (type !== "Bearer" || secretKey !== process.env.SECRET_KEY) {
+    return res.sendStatus(401);
+  }
+
+  var { error } = schema.validate(req.body);
+
+  if (error) {
+    return res.sendStatus(400);
+  }
+
+  res.sendStatus(202);
+  await loader(userId, isServerStartupLoad);
+};
+
+export default resumeAbandonedReportsLoading;
