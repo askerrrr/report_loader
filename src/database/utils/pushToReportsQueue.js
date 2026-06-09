d07bb131880e2eee9562779389db92d644fab64e
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-09 12:50:46 +0300

    Updated the pushToReportsQueue database service: - added the increment of the queueCapacity value by the length of the number of reports Updated the setLoadingProgressStatus database service: - now, when loading is completed, the queueCapacity value has been reset to zero

--- src/database/utils/pushToReportsQueue.js
+++ src/database/utils/pushToReportsQueue.js
@@ -1,7 +1,7 @@
 var pushToReportsQueue = async (collection, userId, periods, session) => {
   await collection.updateOne(
     { userId },
-    { $push: { reportsQueue: { $each: [...periods] } }, $inc: { queueLength: periods.length } },
+    { $push: { reportsQueue: { $each: [...periods] } }, $inc: { queueLength: periods.length, queueCapacity: periods.length } },
     { session: session },
   );
 };
--- src/database/utils/setLoadingProgressStatus.js
+++ src/database/utils/setLoadingProgressStatus.js
@@ -2,11 +2,13 @@
  * @param {"loading" | "completed"} loadingStatus
  */
 var setLoadingProgressStatus = async function (userId, loadingStatus) {
-  var options =
-    loadingStatus === "loading" ? { loadingInProgress: true } : { loadingInProgress: false, lastReportRequestTimestamp: new Date().getTime() };
+  var query =
+    loadingStatus === "loading"
+      ? { loadingInProgress: true }
+      : { loadingInProgress: false, queueCapacity: 0, lastReportRequestTimestamp: new Date().getTime() };
 
   var collection = this;
-  await collection.updateOne({ userId }, { $set: options });
+  await collection.updateOne({ userId }, { $set: query });
 };
 
 export default setLoadingProgressStatus;
