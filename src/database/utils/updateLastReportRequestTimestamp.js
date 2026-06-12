8940aeb7a026cbbec04ea708c3aa5ecc025c2dc6
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-06-12 16:28:21 +0300

    The "updateLastReportRequestTimestamp" method has been updated in the database: - the time is now specified in Moscow time

--- src/database/utils/updateLastReportRequestTimestamp.js
+++ src/database/utils/updateLastReportRequestTimestamp.js
@@ -1,7 +1,7 @@
 var updateLastReportRequestTimestamp = async (collection, userId, session) => {
   var { lastReportRequestTimestamp } = await collection.updateOne(
     { userId },
-    { $set: { lastReportRequestTimestamp: new Date().getTime() } },
+    { $set: { lastReportRequestTimestamp: new Date(Date.now() + 3 * 60 * 60 * 1000).getTime() } },
     { session: session },
   );
   return { lastReportRequestTimestamp };
