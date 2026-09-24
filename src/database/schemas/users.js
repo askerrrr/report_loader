bad1c8c51c581ae662632942949d2c9ef3b3bf59
Asker Sultanov <asker_sultanov1999@mail.ru>
2026-09-24 16:21:17 +0300

    updated users collection schema

--- src/database/schemas/users.js
+++ src/database/schemas/users.js
@@ -3,9 +3,17 @@ import { Schema } from "mongoose";
 var usersSchema = new Schema({
   passwd: { type: String, required: true },
   registeredAt: { type: Date, required: true },
-  login: { type: String, required: true, unique: true },
-  userId: { type: String, required: true, unique: true },
-  role: { type: String, required: true, default: "user", enum: ["user", "admin"] },
+  login: { type: String, required: true },
+  userId: { type: String, required: true },
+  role: {
+    type: String,
+    required: true,
+    default: "user",
+    enum: ["user", "admin"],
+  },
 });
+
+usersSchema.index({ login: 1 }, { unique: true });
+usersSchema.index({ userId: 1 }, { unique: true });
 
 export default usersSchema;
